e81b3f0ec79ce7964ffdadac210fdee3e21af6fa
Benjamin Thorpe <benjaminthorpe19@gmail.com>
2022-07-03 12:19:01 +0000

    add tailwindcss

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,35 @@
 <script>
-  import Output from "./lib/Output.svelte";
-  import Input from "./lib/Input.svelte";
+  import Textarea from "./lib/Textarea.svelte";
+
+  const convert = (sentence) => {
+    let convertedWords = [];
+    const splittedWords = sentence.trim().split(" ");
+
+    splittedWords.forEach((word) => {
+      if (word.length > 1 && word.slice(0, 2) !== "sv") {
+        word = "sv" + word;
+      }
+      convertedWords.push(word);
+    });
+
+    return convertedWords.join(" ");
+  };
+
+  let input = "";
+  $: converted = convert(input);
 </script>
 
-<main>
-  <Input />
-  <Output />
+<main class="bg-slate-200">
+  <div
+    class="h-screen max-w-4xl mx-auto flex flex-col md:flex-row items-center justify-center px-5 gap-5"
+  >
+    <!-- output textarea -->
+    <section class="w-full">
+      <Textarea disabled={true} placeholder="" value={converted} />
+    </section>
+    <!-- input textarea -->
+    <section class="w-full">
+      <Textarea bind:value={input} />
+    </section>
+  </div>
 </main>
---
+++ src/lib/Textarea.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let value = "";
+  export let disabled = false;
+  export let placeholder = "Adds 'sv' in-front of any word in a sentence";
+</script>
+
+<textarea
+  name=""
+  id=""
+  bind:value
+  {disabled}
+  {placeholder}
+  class="p-2 rounded min-h-[150px] min-w-full max-w-full max-h-full shadow-lg
+   focus:outline-gray-300 outline-none text-slate-600 md:min-h-[200px]"
+/>
